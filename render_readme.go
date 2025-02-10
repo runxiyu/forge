778f9acb9907184cda74228ac8d3b76872a17e9f
Runxi Yu <me@runxiyu.org>
2025-02-10 22:31:22 +0800

    render_readme: Use GitHub-flavored markdown

--- render_readme.go
+++ render_readme.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
 )
 
 func render_readme_at_tree(tree *object.Tree) any {
@@ -18,8 +19,15 @@ func render_readme_at_tree(tree *object.Tree) any {
 	if err != nil {
 		return "Unable to fetch contents of README: " + err.Error()
 	}
+
+	md := goldmark.New(
+		goldmark.WithExtensions(
+			extension.GFM,
+		),
+	)
+
 	var readme_rendered_unsafe bytes.Buffer
-	err = goldmark.Convert([]byte(readme_file_contents), &readme_rendered_unsafe)
+	err = md.Convert([]byte(readme_file_contents), &readme_rendered_unsafe)
 	if err != nil {
 		return "Unable to render README: " + err.Error()
 	}
