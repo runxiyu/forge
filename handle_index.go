33594e8b0a24f483353bdd3cfab3ee141ea3a60e
Runxi Yu <me@runxiyu.org>
2025-02-10 13:01:17 +0800

    index: List categories

--- handle_index.go
+++ handle_index.go
@@ -2,7 +2,27 @@ package main
 
 import (
 	"net/http"
+	"os"
 )
 
 func handle_index(w http.ResponseWriter, r *http.Request) {
+	data := make(map[string]any)
+
+	entries, err := os.ReadDir(config.Git.Root)
+	if err != nil {
+		w.Write([]byte("Error listing categories: " + err.Error()))
+		return
+	}
+
+	categories := []string{}
+	for _, entry := range entries {
+		categories = append(categories, entry.Name())
+	}
+	data["categories"] = categories
+
+	err = templates.ExecuteTemplate(w, "index", data)
+	if err != nil {
+		w.Write([]byte("Error rendering template: " + err.Error()))
+		return
+	}
 }
