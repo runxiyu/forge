86f9d62c5ef3e61fdac9bda0a9920b8c1d9820fa
Runxi Yu <me@runxiyu.org>
2025-02-12 11:28:37 +0800

    handle_index: Query group names from database

--- handle_index.go
+++ handle_index.go
@@ -2,22 +2,33 @@ package main
 
 import (
 	"net/http"
-	"os"
 )
 
 func handle_index(w http.ResponseWriter, r *http.Request) {
 	data := make(map[string]any)
 
-	entries, err := os.ReadDir(config.Git.Root)
+	rows, err := database.Query(r.Context(), "SELECT name FROM groups")
 	if err != nil {
-		_, _ = w.Write([]byte("Error listing groups: " + err.Error()))
+		_, _ = w.Write([]byte("Error querying groups: " + err.Error()))
+		return
+	}
+	defer rows.Close()
+
+	groups := []string{}
+	for rows.Next() {
+		var groupName string
+		if err := rows.Scan(&groupName); err != nil {
+			_, _ = w.Write([]byte("Error scanning group name: " + err.Error()))
+			return
+		}
+		groups = append(groups, groupName)
+	}
+
+	if err := rows.Err(); err != nil {
+		_, _ = w.Write([]byte("Error iterating over rows: " + err.Error()))
 		return
 	}
 
-	groups := []string{}
-	for _, entry := range entries {
-		groups = append(groups, entry.Name())
-	}
 	data["groups"] = groups
 
 	err = templates.ExecuteTemplate(w, "index", data)
