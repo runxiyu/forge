d8cfbf4d10788ef0f3bf730d824734c496602b0c
Runxi Yu <me@runxiyu.org>
2025-02-13 10:54:01 +0800

    group_index: Use table list

--- http_handle_group_index.go
+++ http_handle_group_index.go
@@ -7,16 +7,33 @@ import (
 func handle_group_repos(w http.ResponseWriter, r *http.Request, params map[string]any) {
 	group_name := params["group_name"]
 
-	names, err := query_list[string](r.Context(), "SELECT r.name FROM repos r JOIN groups g ON r.group_id = g.id WHERE g.name = $1;", group_name)
+	rows, err := database.Query(r.Context(), "SELECT r.name, COALESCE(r.description, '') FROM repos r JOIN groups g ON r.group_id = g.id WHERE g.name = $1;", group_name)
 	if err != nil {
-		http.Error(w, "Error getting groups:: "+err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Error getting groups: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
-	params["repos"] = names
+	defer rows.Close()
+
+	repos := []struct {
+		Name        string
+		Description string
+	}{}
+	for rows.Next() {
+		var repoName, repoDescription string
+		if err := rows.Scan(&repoName, &repoDescription); err != nil {
+			http.Error(w, "Error scanning repo: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		repos = append(repos, struct {
+			Name        string
+			Description string
+		}{repoName, repoDescription})
+	}
+	params["repos"] = repos
 
 	err = templates.ExecuteTemplate(w, "group_repos", params)
 	if err != nil {
-		http.Error(w, "Error rendering template:: "+err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Error rendering template: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
