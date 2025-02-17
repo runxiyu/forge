52c8e56fe7da34a449ba0e497325f9cf27834721
Runxi Yu <me@runxiyu.org>
2025-02-17 22:37:38 +0800

    remote_url.go: Don't use path.Join (it strips :// into :/)

--- remote_url.go
+++ remote_url.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"net/url"
-	"path"
+	"strings"
 )
 
 func generate_ssh_remote_url(group_name, repo_name string) string {
-	return path.Join(config.SSH.Root, url.PathEscape(group_name), "/:/repos/", url.PathEscape(repo_name))
+	return strings.TrimSuffix(config.SSH.Root, "/") + "/" + url.PathEscape(group_name) + "/:/repos/" + url.PathEscape(repo_name)
 }
 
 func generate_http_remote_url(group_name, repo_name string) string {
-	return path.Join(config.HTTP.Root, url.PathEscape(group_name), "/:/repos/", url.PathEscape(repo_name))
+	return strings.TrimSuffix(config.HTTP.Root, "/") + "/" + url.PathEscape(group_name) + "/:/repos/" + url.PathEscape(repo_name)
 }
