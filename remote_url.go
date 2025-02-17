5c80236c0ed27af9db42b6d5b60f0d365457c7a4
Runxi Yu <me@runxiyu.org>
2025-02-17 12:05:52 +0800

    {remote_url,ssh_server}.go: Use path.Join instead of trimming and adding

--- remote_url.go
+++ remote_url.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"net/url"
-	"strings"
+	"path"
 )
 
 func generate_ssh_remote_url(group_name, repo_name string) string {
-	return strings.TrimSuffix(config.SSH.Root, "/") + "/" + url.PathEscape(group_name) + "/:/repos/" + url.PathEscape(repo_name)
+	return path.Join(config.SSH.Root, url.PathEscape(group_name), "/:/repos/", url.PathEscape(repo_name))
 }
 
 func generate_http_remote_url(group_name, repo_name string) string {
-	return strings.TrimSuffix(config.HTTP.Root, "/") + "/" + url.PathEscape(group_name) + "/:/repos/" + url.PathEscape(repo_name)
+	return path.Join(config.HTTP.Root, url.PathEscape(group_name), "/:/repos/", url.PathEscape(repo_name))
 }
