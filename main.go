6b1286fa7a50facd0dc3dfb4ed4b004ed3ce6d11
Runxi Yu <me@runxiyu.org>
2025-02-17 14:06:00 +0800

    main.go: Restructure for uniformity

--- main.go
+++ main.go
@@ -16,57 +16,51 @@ func main() {
 	)
 	flag.Parse()
 
-	err := load_config(*config_path)
-	if err != nil {
+	if err := load_config(*config_path); err != nil {
 		clog.Fatal(1, "Loading configuration: "+err.Error())
 	}
-
-	err = deploy_hooks_to_filesystem()
-	if err != nil {
+	if err := deploy_hooks_to_filesystem(); err != nil {
 		clog.Fatal(1, "Deploying hooks to filesystem: "+err.Error())
 	}
-
-	err = load_templates()
-	if err != nil {
+	if err := load_templates(); err != nil {
 		clog.Fatal(1, "Loading templates: "+err.Error())
 	}
 
+	// UNIX socket listener for hooks
+	hooks_listener, err := net.Listen("unix", config.Hooks.Socket)
+	if err != nil {
+		clog.Fatal(1, "Listening hooks: "+err.Error())
+	}
+	clog.Info("Listening hooks on unix " + config.Hooks.Socket)
+	go func() {
+		if err := serve_git_hooks(hooks_listener); err != nil {
+			clog.Fatal(1, "Serving hooks: "+err.Error())
+		}
+	}()
+
+	// SSH listener
 	ssh_listener, err := net.Listen(config.SSH.Net, config.SSH.Addr)
 	if err != nil {
 		clog.Fatal(1, "Listening SSH: "+err.Error())
 	}
 	clog.Info("Listening SSH on " + config.SSH.Net + " " + config.SSH.Addr)
 	go func() {
-		err = serve_ssh(ssh_listener)
-		if err != nil {
+		if err := serve_ssh(ssh_listener); err != nil {
 			clog.Fatal(1, "Serving SSH: "+err.Error())
 		}
 	}()
 
-	hooks_listener, err := net.Listen("unix", config.Hooks.Socket)
-	if err != nil {
-		clog.Fatal(1, "Listening hooks: "+err.Error())
-	}
-	clog.Info("Listening hooks at " + config.Hooks.Socket)
-	go func() {
-		err = serve_git_hooks(hooks_listener)
-		if err != nil {
-			clog.Fatal(1, "Serving hooks: "+err.Error())
-		}
-	}()
-
-	listener, err := net.Listen(config.HTTP.Net, config.HTTP.Addr)
+	// HTTP listener
+	http_listener, err := net.Listen(config.HTTP.Net, config.HTTP.Addr)
 	if err != nil {
 		clog.Fatal(1, "Listening HTTP: "+err.Error())
 	}
 	clog.Info("Listening HTTP on " + config.HTTP.Net + " " + config.HTTP.Addr)
-
 	go func() {
-		err = http.Serve(listener, &http_router_t{})
-		if err != nil {
+		if err := http.Serve(http_listener, &http_router_t{}); err != nil {
 			clog.Fatal(1, "Serving HTTP: "+err.Error())
 		}
 	}()
 
-	select{}
+	select {}
 }
