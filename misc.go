ebd3c3411dfc0ee5e2947f0f6352d819cf24bcc9
Runxi Yu <me@runxiyu.org>
2025-02-17 12:00:31 +0800

    misc.go: Remove environ_to_map as unused

--- misc.go
+++ misc.go
@@ -9,17 +9,3 @@ type name_desc_t struct {
 	Name        string
 	Description string
 }
-
-var err_environ_no_separator = errors.New("No separator found in environ line")
-
-func environ_to_map(environ_strings []string) (result map[string]string, err error) {
-	result = make(map[string]string)
-	for _, environ_string := range environ_strings {
-		key, value, found := strings.Cut(environ_string, "=")
-		if !found {
-			return result, err_environ_no_separator
-		}
-		result[key] = value
-	}
-	return result, err_environ_no_separator
-}
