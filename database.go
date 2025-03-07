54d9a5a652c57546e5d9aa30cc58ab2c24a742d9
Runxi Yu <me@runxiyu.org>
2025-03-07 08:09:27 +0800

    database: Remove query_list

--- database.go
+++ database.go
@@ -9,31 +9,6 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-// query_list is a helper function that executes a query and returns a list of
-// results.
-func query_list[T any](ctx context.Context, query string, args ...any) (result []T, err error) {
-	var rows pgx.Rows
-
-	if rows, err = database.Query(ctx, query, args...); err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var item T
-		if err = rows.Scan(&item); err != nil {
-			return nil, err
-		}
-		result = append(result, item)
-	}
-
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
-
 // query_name_desc_list is a helper function that executes a query and returns a
 // list of name_desc_t results.
 func query_name_desc_list(ctx context.Context, query string, args ...any) (result []name_desc_t, err error) {
