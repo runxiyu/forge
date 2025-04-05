38b24afcb653da88765c99049f850c3ec7e234db
Runxi Yu <me@runxiyu.org>
2025-04-05 23:22:51 +0800

    misc: Move IterSeqLimit to misc

--- internal/misc/iter.go
+++ internal/misc/iter.go
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 // SPDX-FileCopyrightText: Copyright (c) 2025 Runxi Yu <https://runxiyu.org>
 
-package forge
+package misc
 
 import "iter"
 
 // iterSeqLimit returns an iterator equivalent to the supplied one, but stops
 // after n iterations.
-func iterSeqLimit[T any](s iter.Seq[T], n uint) iter.Seq[T] {
+func IterSeqLimit[T any](s iter.Seq[T], n uint) iter.Seq[T] {
 	return func(yield func(T) bool) {
 		var iterations uint
 		for v := range s {
