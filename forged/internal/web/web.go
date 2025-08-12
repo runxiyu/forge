dd6e40c759004c4c15944aed1289358bda038848
Runxi Yu <me@runxiyu.org>
2025-08-12 11:01:07 +0800

    Remove the mess

--- forged/internal/web/web.go
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-// SPDX-FileCopyrightText: Copyright (c) 2025 Runxi Yu <https://runxiyu.org>
-
-// Package web provides web-facing components of the forge.
-package web
