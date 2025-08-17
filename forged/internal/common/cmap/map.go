3142c4ee05a2094997a96deb4bd1f33123143b31
Runxi Yu <me@runxiyu.org>
2025-08-17 14:14:19 +0800

    License cleanup

--- forged/internal/common/cmap/map.go
+++ forged/internal/common/cmap/map.go
@@ -3,7 +3,7 @@
 // Copyright 2024 Runxi Yu (porting it to generics)
 // Copyright 2016 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE.BSD file.
+// license that can be found in the LICENSE file.
 
 // Package cmap provides a generic Map safe for concurrent use.
 package cmap
